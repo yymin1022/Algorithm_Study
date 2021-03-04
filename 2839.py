41ecb82249506c2615d531be53b11a7022483792
yymin1022 <yymin1022@gmail.com>
2021-03-04 09:45:56 +0000

    Update 2839

---
+++ 2839.py
@@ -0,0 +1,21 @@
+weight = int(input())
+count = 0
+
+if weight % 5 == 0:
+    print(int(weight / 5))
+else:
+    while(True):
+        count += 1
+        weight -= 3
+
+        if weight < 0:
+            print(-1)
+            break
+
+        if weight == 0:
+            print(count)
+            break
+
+        if weight % 5 == 0:
+            print(count + int(weight / 5))
+            break
