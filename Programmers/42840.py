01342cc090b947eb28a2ea67f848fea2b3306e18
yymin1022 <yymin1022@gmail.com>
2021-03-06 05:35:22 +0000

    Update Programmers 42840

---
+++ Programmers/42840.py
@@ -0,0 +1,25 @@
+def solution(answers):
+    answer = []
+    stu1 = [1, 2, 3, 4, 5] * 8
+    stu2 = [2, 1, 2, 3, 2, 4, 2, 5] * 5
+    stu3 = [3, 3, 1, 1, 2, 2, 4, 4›, 5, 5] * 4
+    
+    scores = [0, 0, 0]
+    
+    for i in range(len(answers)):
+        if answers[i] == stu1[i%len(stu1)]:
+            scores[0] += 1
+        if answers[i] == stu2[i%len(stu2)]:
+            scores[1] += 1
+        if answers[i] == stu3[i%len(stu3)]:
+            scores[2] += 1
+        
+    scoreMax = max(scores)
+    
+    for i in range(len(scores)):
+        if scoreMax == scores[i]:
+            answer.append(i + 1)
+        
+    answer.sort()
+    
+    return answer
