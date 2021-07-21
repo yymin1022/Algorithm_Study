e0b00b5f56f454dd67f62b4ce1d8af26474b33f6
yymin1022 <yymin1022@gmail.com>
2021-07-21 05:21:08 +0000

    Update BOJ 2562

---
+++ BOJ/2562.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int n[9], MAX, index;
+
+    MAX = 0;
+
+    for(int i = 0; i < 9; i++){
+        scanf("%d", (n + i));
+
+        if(n[i] > MAX){
+            MAX = n[i];
+            index = i + 1;
+        }
+    }
+
+    printf("%d\n%d\n", MAX, index);
+
+    return 0;
+}
