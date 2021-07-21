5103adfbdd90b0b658fda8aa21fc63ee593db2d5
yymin1022 <yymin1022@gmail.com>
2021-07-21 04:39:34 +0000

    Update BOJ 2438

---
+++ BOJ/2438.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int input;
+    scanf("%d", &input);
+
+    for(int i = 1; i <= input; i++){
+        for(int j = 0; j < i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
