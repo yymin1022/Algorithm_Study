3d6df7e020d4be6b72a7150549a48916c2e74a67
yymin1022 <yymin1022@gmail.com>
2021-07-21 05:14:20 +0000

    Update BOJ 10818

---
+++ BOJ/10818.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int n, MAX, MIN;
+    
+    scanf("%d", &n);
+
+    int nums[n];
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", (nums + i));
+
+        if(i == 0){
+            MAX = nums[0];
+            MIN = nums[0];
+        }else{
+            if(MAX < nums[i]){
+                MAX = nums[i];
+            }
+            if(MIN > nums[i]){
+                MIN = nums[i];
+            }
+        }
+    }
+
+    printf("%d %d\n", MIN, MAX);
+
+    return 0;
+}
