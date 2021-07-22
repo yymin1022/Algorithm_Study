f1a1154e459e05c3e0a228b1f4b5ebb179a6a7a5
yymin1022 <yymin1022@gmail.com>
2021-07-22 14:19:03 +0000

    Update BOJ 1546

---
+++ BOJ/1546.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int count, max = 0;
+
+    scanf("%d", &count);
+
+    int nums[count];
+    float values[count], sum = 0.0;
+
+    for(int i = 0; i < count; i++){
+        scanf("%d", nums + i);
+
+        if(nums[i] > max){
+            max = nums[i];
+        }
+    }
+
+    for(int i = 0; i < count; i++){
+        values[i] = (double)nums[i] / (double)max * 100;
+        sum += values[i];
+    }
+
+    printf("%f", sum/count);
+
+    return 0;
+}
