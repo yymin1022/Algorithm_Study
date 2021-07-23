b9d0a4e47573f125a7fe76f864e7c07f4f9ad523
yymin1022 <yymin1022@gmail.com>
2021-07-23 02:54:18 +0000

    Update BOJ 4344

---
+++ BOJ/4344.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    int count;
+
+    scanf("%d", &count);
+
+    for(int i = 0; i < count; i++){
+        int num, sum = 0;
+        scanf("%d", &num);
+
+        int score[num];
+        for(int j = 0; j < num; j++){
+            scanf("%d", score + j);
+            sum += score[j];
+        }
+
+        float avg = (double)sum / (double)num;
+        int students = 0;
+
+        for(int j = 0; j < num; j++){
+            if(score[j] > avg){
+                students++;
+            }
+        }
+
+        printf("%.3f%%\n", (double)students / (double)num * 100);
+    }
+
+    return 0;
+}
