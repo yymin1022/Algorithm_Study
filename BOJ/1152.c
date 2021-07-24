33ea5ec6c335cf59d6ca3a16d234e9835ead9846
yymin1022 <yymin1022@gmail.com>
2021-07-24 14:02:19 +0000

    Update BOJ 1152

---
+++ BOJ/1152.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char input[1000000];
+    int count = 1;
+
+    scanf("%[^\n]", input);
+
+    if(*(input) == ' ' && *(input + 1) == '\0'){
+        printf("0");
+
+        return 0;
+    }
+
+    for(int i = 1; i < 1000001; i++){
+        if(*(input + i) == '\0'){
+            break;
+        }
+
+        if(*(input + i) == ' ' && *(input + i + 1) != '\0'){
+            count++;
+        }
+    }
+
+    printf("%d", count);
+    
+    return 0;
+}
