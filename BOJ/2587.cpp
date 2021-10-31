15b4c7f60239998fde6ff399c7bca2e67ff58006
yymin1022 <yymin1022@gmail.com>
2021-10-31 20:03:09 +0900

    Update BOJ 2587

---
+++ BOJ/2587.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int sum = 0;
+    vector<int> nums;
+
+    for(int i = 0; i < 5; i++){
+        int input;
+        cin >> input;
+        nums.push_back(input);
+        sum += input;
+    }
+
+    sort(nums.begin(), nums.end());
+
+    cout << sum / 5 << "\n" << nums[2];
+
+    return 0;
+}
