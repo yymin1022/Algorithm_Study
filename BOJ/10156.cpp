adea0660dce669fac8fb594983732b5cb8cd958a
yymin1022 <yymin1022@gmail.com>
2021-11-02 15:22:42 +0900

    Update BOj 10156

---
+++ BOJ/10156.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int val, num, cur;
+
+    cin >> val >> num >> cur;
+
+    int result = val * num - cur;
+
+    if(result > 0){
+        cout << result;
+    }else{
+        cout << 0;
+    }
+
+
+    return 0;
+}
