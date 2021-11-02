2fb39cc4ef5e8913fc7dbb92f98974d3e0770f3b
yymin1022 <yymin1022@gmail.com>
2021-11-02 15:28:51 +0900

    Update BOJ 7567

---
+++ BOJ/7567.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int size = 0;;
+    string input;
+    cin >> input;
+
+    for(int i = 0; i < input.size(); i++){
+        if(i != 0){
+            if(input[i] != input[i - 1]){
+                size += 10;
+            }else{
+                size += 5;
+            }
+        }else{
+            size += 10;
+        }
+    }
+
+    cout << size;
+
+    return 0;
+}
