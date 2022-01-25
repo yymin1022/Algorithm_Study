32544f7cf8d14bcc08afcf15c98b74370a712cd1
yymin1022 <yymin1022@gmail.com>
2022-01-25 16:15:56 +0900

    Updaet BOJ 2558

---
+++ BOJ/2558.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(0);
+    
+    int A, B;
+    cin >> A >> B;
+    
+    cout << A + B;
+    
+    return 0;
+}
