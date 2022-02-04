7964b45d9208c0cd3912e62c48a58c9f500a0deb
yymin1022 <yymin1022@gmail.com>
2022-02-04 14:43:45 +0900

    Update BOJ 10926

---
+++ BOJ/10926.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(0);
+
+    string name;
+    cin >> name;
+
+    cout << name << "??!";
+
+    return 0;
+}
