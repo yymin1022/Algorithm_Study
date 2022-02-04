ac062f8be39065212df7fa7354e3fe7a693edac9
yymin1022 <yymin1022@gmail.com>
2022-02-04 14:50:49 +0900

    Update Solving Post : BOj 18108

---
+++ BOJ/18108.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(0);
+
+    int year;
+    cin >> year;
+    
+    cout << year - 543;
+
+    return 0;
+}
