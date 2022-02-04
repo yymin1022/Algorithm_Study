b53e8f690aacb5f87622d2ad8d6d5ac1109937d6
yymin1022 <yymin1022@gmail.com>
2022-02-04 23:05:23 +0900

    Update BOJ 4673

---
+++ BOJ/4673.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int nums[10001] = {};
+
+int d(int n){
+    return n / 1000 + n % 1000 / 100 + n % 100 / 10 + n % 10 + n;
+}
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(0);
+
+    for(int i = 1; i < 10000; i++){
+        int num = d(i);
+        if(num < 10000){
+            nums[num] = 1;
+        }
+    }
+
+    for(int i = 1; i < 10000; i++){
+        if(nums[i] != 1){
+            cout << i << "\n";
+        }
+    }
+}
