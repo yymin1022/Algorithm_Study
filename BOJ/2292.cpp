eb1cd3cbbe73f32ca9f25ac094eb5b06774b0127
yymin1022 <yymin1022@gmail.com>
2022-02-04 21:04:14 +0900

    Update BOJ 2292

---
+++ BOJ/2292.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(0);
+
+    int N;
+    cin >> N;
+
+    int count = 2;
+    int i = 1;
+    while(count <= N){
+        count += i * 6;
+        i++;
+    }
+
+    if(N == 1){
+        cout << 1;
+    }else{
+        cout << i;
+    }
+
+    return 0;
+}
