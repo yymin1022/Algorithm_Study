b50343d22b51695fcc7255c8e9cbb14fb9a6e2ab
yymin1022 <yymin1022@gmail.com>
2022-02-05 14:47:42 +0900

    Update BOJ 2869

---
+++ BOJ/2869.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(0);
+
+    int A, B, V;
+    cin >> A >> B >> V;
+
+    int day = (V - A) / (A - B) + 1;
+
+    if((V - A) % (A - B) != 0){
+        day++;
+    }
+
+    cout << day;
+}
