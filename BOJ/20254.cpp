4b961b6e02f5e391bf83ddc60a01b6ecc9a375d6
yymin1022 <yymin1022@gmail.com>
2022-02-09 22:12:28 +0900

    Update BOJ 20254

---
+++ BOJ/20254.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int Ur, Tr, Uo, To;
+    cin >> Ur >> Tr >> Uo >> To;
+
+    cout << Ur * 56 + Tr * 24 + Uo * 14 + To * 6 << "\n";
+
+    return 0;
+}
