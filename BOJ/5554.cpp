6baf9ef13b12e33da7c620050dd72bd58923a59e
yymin1022 <yymin1022@gmail.com>
2022-02-09 11:27:06 +0900

    Update BOJ 5554

---
+++ BOJ/5554.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int time = 0;
+    for(int i = 0; i < 4; i++){
+        int input;
+        cin >> input;
+        time += input;
+    }
+
+    cout << time / 60 << "\n" << time % 60 << "\n";
+
+    return 0;
+}
