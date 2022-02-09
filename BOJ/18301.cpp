5f5cc740f07d920ce5f7d231c0e8a9aeb5180797
yymin1022 <yymin1022@gmail.com>
2022-02-09 21:26:46 +0900

    Update BOJ 18301

---
+++ BOJ/18301.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n1, n2, n12;
+    cin >> n1 >> n2 >> n12;
+
+    cout << (n1 + 1) * (n2 + 1) / (n12 + 1) - 1 << "\n";
+
+    return 0;
+}
