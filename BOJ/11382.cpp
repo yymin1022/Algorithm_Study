456ab97558f1de9ce3ea76eef15d7691283137f8
yymin1022 <yymin1022@gmail.com>
2022-02-09 20:16:16 +0900

    Update BOJ 11382

---
+++ BOJ/11382.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    long long sum = 0;
+    for(int i = 0; i < 3; i++){
+        long long num;
+        cin >> num;
+        sum += num;
+    }
+
+    cout << sum << "\n";
+
+    return 0;
+}
