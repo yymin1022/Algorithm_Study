e08d3a605e04ac882cbd9a9dc3b22e717822c56f
yymin1022 <yymin1022@gmail.com>
2022-02-09 22:42:18 +0900

    Update BOJ 24078

---
+++ BOJ/24078.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int X;
+    cin >> X;
+
+    cout << X % 21 << "\n";
+
+    return 0;
+}
