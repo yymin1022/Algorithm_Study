8447d722dd6b4e4348e8be2ba32478100612f605
yymin1022 <yymin1022@gmail.com>
2022-02-09 23:32:08 +0900

    Update BOJ 1237

---
+++ BOJ/1237.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cout << "문제의 정답" << "\n";
+
+    return 0;
+}
