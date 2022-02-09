e31f789cd16ac45ceabfdb399c10a81bbc7f759b
yymin1022 <yymin1022@gmail.com>
2022-02-09 22:47:50 +0900

    Update BOJ 24082

---
+++ BOJ/24082.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int X;
+    cin >> X;
+
+    cout << (long long)pow(X, 3) << "\n";
+
+    return 0;
+}
