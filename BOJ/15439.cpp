bf3d0ddadd9ce0fd1bc7b69e45081a4e2a386eaf
yymin1022 <yymin1022@gmail.com>
2022-02-09 20:39:49 +0900

    Update BOJ 15439

---
+++ BOJ/15439.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    cout << N * (N - 1) << "\n";
+    
+    return 0;
+}
