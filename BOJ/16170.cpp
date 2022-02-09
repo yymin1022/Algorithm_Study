b1e1a18272ecc69bc5e863b7a0952ea337d22cef
yymin1022 <yymin1022@gmail.com>
2022-02-09 21:00:02 +0900

    Update BOJ 16170

---
+++ BOJ/16170.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    char curTime[20];
+    time_t now = time(0);
+    struct tm  tstruct;
+    tstruct = *gmtime(&now);
+    strftime(curTime, 20, "%Y\n%m\n%d", &tstruct);
+
+    cout << curTime << "\n";
+
+    return 0;
+}
