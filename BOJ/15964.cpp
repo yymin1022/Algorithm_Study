844d9f400c6ee74e2153ac5cc52a7fae38a01b78
yymin1022 <yymin1022@gmail.com>
2022-02-09 20:55:29 +0900

    Update BOJ 15964

---
+++ BOJ/15964.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    long long A, B;
+    cin >> A >> B;
+
+    cout << (A + B) * (A - B) << "\n";
+    
+    return 0;
+}
