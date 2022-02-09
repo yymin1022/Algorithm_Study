a3cbb8d227ab8f69504792ea1e96ca379466c0ec
yymin1022 <yymin1022@gmail.com>
2022-02-09 11:50:06 +0900

    Update BOJ 6749

---
+++ BOJ/6749.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int Y, M;
+    cin >> Y >> M;
+    cout << M * 2 - Y << "\n";
+
+    return 0;
+}
