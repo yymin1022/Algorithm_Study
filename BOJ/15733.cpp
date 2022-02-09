8bbd173cda666713fee37c0508d203df83fed40d
yymin1022 <yymin1022@gmail.com>
2022-02-09 20:42:34 +0900

    Update BOJ 15733

---
+++ BOJ/15733.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cout << "I'm Sexy" << "\n";
+    
+    return 0;
+}
