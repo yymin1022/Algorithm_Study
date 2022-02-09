bb4b60919ffebd2b5bff54826ced642f417ae032
yymin1022 <yymin1022@gmail.com>
2022-02-09 21:10:19 +0900

    Update BOJ 17295

---
+++ BOJ/17295.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cout << "Avengers: Endgame" << "\n";
+
+    return 0;
+}
