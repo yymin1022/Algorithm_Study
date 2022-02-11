6d631537be169f0b637ee976bd8537180a08e739
yymin1022 <yymin1022@gmail.com>
2022-02-11 16:47:38 +0900

    Update BOJ 1427

---
+++ BOJ/1427.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    vector<int> nums;
+    while(N){
+        nums.push_back(N % 10);
+        N /= 10;
+    }
+
+    sort(nums.rbegin(), nums.rend());
+
+    for(int i = 0; i < nums.size(); i++){
+        cout << nums[i];
+    }
+
+    return 0;
+}
