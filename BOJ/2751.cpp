f3b3e6beb07c0e43d6a919f6a5c14a3347682e02
yymin1022 <yymin1022@gmail.com>
2022-02-11 16:14:34 +0900

    Update BOJ 2751

---
+++ BOJ/2751.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    vector<int> nums;
+    for(int i = 0; i < N; i++){
+        int input;
+        cin >> input;
+
+        nums.push_back(input);
+    }
+
+    sort(nums.begin(), nums.end());
+
+    for(int i = 0; i < N; i++){
+        cout << nums[i] << "\n";
+    }
+
+    return 0;
+}
