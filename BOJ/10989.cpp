309c6610e6e59217f248a6d3dab30057a43ee931
yymin1022 <yymin1022@gmail.com>
2022-02-11 16:25:19 +0900

    Update BOJ 10989

---
+++ BOJ/10989.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int nums[10001];
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        int input;
+        cin >> input;
+
+        nums[input]++;
+    }
+
+    for(int i = 1; i <= 10000; i++){
+        for(int j = 0; j < nums[i]; j++){
+            cout << i << "\n";
+        }
+    }
+
+    return 0;
+}
