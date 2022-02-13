b1da66616307b3fadb6d467a1cbab0c9a1af2de8
yymin1022 <yymin1022@gmail.com>
2022-02-13 17:01:52 +0900

    Update BOJ 1920

---
+++ BOJ/1920.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    vector<int> A;
+    for(int i = 0; i < N; i++){
+        int input;
+        cin >> input;
+        A.push_back(input);
+    }
+
+    sort(A.begin(), A.end());
+
+    int M;
+    cin >> M;
+    for(int i = 0; i < M; i++){
+        int input;
+        cin >> input;
+
+        cout << binary_search(A.begin(), A.end(), input) << "\n";
+    }
+    
+    return 0;
+}
