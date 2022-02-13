2af0ebc41817d149b7819036d3a3b65526829b3e
yymin1022 <yymin1022@gmail.com>
2022-02-13 14:49:04 +0900

    Update Solving Post : BOJ 11050

---
+++ BOJ/11050.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, K;
+    cin >> N >> K;
+
+    int result = 1;
+    for(int i = 0; i < K; i++){
+        result *= N;
+        N--;
+    }
+
+    while(K){
+        result /= K;
+        K--;
+    }
+
+    cout << result << "\n";
+    
+    return 0;
+}
