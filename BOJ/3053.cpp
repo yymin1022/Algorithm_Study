04e70b52edca91b18a701e884d0e25c112bad08c
yymin1022 <yymin1022@gmail.com>
2022-02-14 17:52:06 +0900

    Update BOJ 3053

---
+++ BOJ/3053.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    double R;
+    cin >> R;
+
+    cout << fixed;
+    cout.precision(6);
+    cout << R * R * 3.14159265358979 << "\n";
+    cout << R * R * 2 << "\n";
+
+    return 0;
+}
