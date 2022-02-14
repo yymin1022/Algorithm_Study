91dc782ac50ee5b92b64a18dc429f64cf839fb60
yymin1022 <yymin1022@gmail.com>
2022-02-14 11:40:41 +0900

    Update BOJ 4948

--- BOJ/4948.cpp
+++ BOJ/4948.cpp
@@ -2,26 +2,25 @@
 
 using namespace std;
 
-bool nums[250001];
-int sum[250001];
-
 int main(){
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    nums[0] = nums[1] = true;
+    vector<bool> nums(250000, true);
+    nums[0] = nums[1] = false;
     for(int i = 2; i < 250000; i++){
-        if(nums[i]){
+        if(!nums[i]){
             continue;
         }
         for(int j = i * 2; j < 250000; j += i){
-            nums[j] = true;
+            nums[j] = false;
         }
     }
 
+    vector<int> sum(250000, 0);
     for(int i = 2; i < 250000; i++){
-        if(!nums[i]){
+        if(nums[i]){
             sum[i]++;
         }
         sum[i] += sum[i - 1];
