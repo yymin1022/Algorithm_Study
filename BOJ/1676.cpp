e675099e5b40952628bddabf447302fb51c4a3a0
yymin1022 <yymin1022@gmail.com>
2022-02-24 14:34:13 +0900

    Update BOJ 1676

---
+++ BOJ/1676.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    int cnt = 0;
+    for(int i = N; i > 0; i--){
+        int temp = i;
+
+        while(temp){
+            if(temp % 5 == 0){
+                cnt++;
+                temp /= 5;
+            }else{
+                break;
+            }
+        }
+    }
+
+    cout << cnt << "\n";
+
+    return 0;
+}
