d30ad47dbdc0f940379074681e12f0d265d93de3
yymin1022 <yymin1022@gmail.com>
2022-02-28 15:13:30 +0900

    Update BOJ 1927

---
+++ BOJ/1927.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    priority_queue<int> nums;
+    for(int i = 0; i < N; i++){
+        int cmd;
+        cin >> cmd;
+
+        if(cmd == 0){
+            if(nums.empty()){
+                cout << 0 << "\n";
+            }else{
+                cout << nums.top() * -1 << "\n";
+                nums.pop();
+            }
+        }else{
+            nums.push(cmd * -1);
+        }
+    }
+
+    return 0;
+}
