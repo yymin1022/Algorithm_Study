9ec6c6220aae88fbc001e537b5ab103925d20da3
yymin1022 <yymin1022@gmail.com>
2022-03-09 21:00:48 +0900

    Update BOJ 1009

---
+++ BOJ/1009.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+
+    for(int i = 0; i < T; i++){
+        int a, b;
+        cin >> a >> b;
+
+        int value = 1;
+
+        for(int i = 0; i < b; i++){
+            value *= a;
+            value %= 10;
+        }
+
+        if(value == 0){
+            cout << 10 << "\n";
+        }else{
+            cout << value << "\n";
+        }
+    }
+
+    return 0;
+}
