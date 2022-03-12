b2f21d5b5634da7185840cd6cbebe8f2bd317038
yymin1022 <yymin1022@gmail.com>
2022-03-13 07:39:09 +0900

    Update BOJ 6064

---
+++ BOJ/6064.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+
+    for(int i = 0; i < T; i++){
+        int M, N, X, Y;
+        cin >> M >> N >> X >> Y;
+
+        X--;
+        Y--;
+        
+        bool isRight = false;
+        for(int j = X; j < M * N; j += M){
+            if(j % N == Y){
+                cout << j + 1 << "\n";
+                isRight = true;
+                break;
+            }
+        }
+
+        if(!isRight){
+            cout << -1 << "\n";
+        }
+    }
+
+    return 0;
+}
