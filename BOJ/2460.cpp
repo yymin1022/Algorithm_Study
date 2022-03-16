99c8adac710d69037d40220905ccfa5883b98349
yymin1022 <yymin1022@gmail.com>
2022-03-16 10:43:25 +0900

    Update BOJ 2460

---
+++ BOJ/2460.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int max = 0;
+    int num = 0;
+
+    for(int i = 0; i < 10; i++){
+        int in, out;
+        cin >> out >> in;
+
+        num -= out;
+        num += in;
+
+        if(max < num){
+            max = num;
+        }
+    }
+
+    cout << max;
+    
+    return 0;
+}
