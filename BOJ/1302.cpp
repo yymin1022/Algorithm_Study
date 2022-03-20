f5b003b02c598a007e3414cb224de8328257b11e
yymin1022 <yymin1022@gmail.com>
2022-03-21 07:13:27 +0900

    Update BOJ 1302

---
+++ BOJ/1302.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    int maxCnt = 0;
+    map<string, int> books;
+    for(int i = 0; i < N; i++){
+        string title;
+        cin >> title;
+        books[title]++;
+
+        if(books[title] > maxCnt){
+            maxCnt = books[title];
+        }
+    }
+
+    for(auto temp = books.begin(); temp != books.end(); temp++){
+        if(temp->second == maxCnt){
+            cout << temp->first << "\n";
+            break;
+        }
+    }
+}
