3008227762d4781c21f214d4d412e7fd8c861a0c
yymin1022 <yymin1022@gmail.com>
2022-04-07 16:42:03 +0900

    Update BOJ 13305

---
+++ BOJ/13305.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long cost[100001];
+long long dist[100001];
+
+int main(void){
+    cin.tie(NULL);
+    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    int N;
+	cin >> N;
+	for(int i = 0; i < N - 1; i++){
+        cin >> dist[i];
+    }
+	
+    for(int i = 0; i < N; i++){
+        cin >> cost[i];
+    }
+
+    long long minCost = 987654321;
+    long long sumCost = 0;
+    for(int i = 0; i < N - 1; i++){
+		if(cost[i] < minCost){
+            minCost = cost[i];
+        }
+
+		sumCost += minCost * dist[i];
+	}
+
+    cout << sumCost << "\n";
+
+	return 0;
+}
