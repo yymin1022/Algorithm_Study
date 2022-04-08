b27b69abdbf1f317a4b5dacf0110341e4afbf8fc
yymin1022 <yymin1022@gmail.com>
2022-04-08 20:47:01 +0900

    Update BOJ 5086

---
+++ BOJ/5086.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    cin.tie(NULL);
+    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    int a, b;
+    cin >> a >> b;
+    while(a != 0 && b != 0){
+        if(a < b && b % a == 0){
+            cout << "factor" << "\n";
+        }else if(a > b && a % b == 0){
+            cout << "multiple" << "\n";
+        }else{
+            cout << "neither" << "\n";
+        }
+
+        cin >> a >> b;
+    }
+
+	return 0;
+}
