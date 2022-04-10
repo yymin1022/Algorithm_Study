2c001130fab1ef6702bc285555675c7e8cd62cb3
yymin1022 <yymin1022@gmail.com>
2022-04-10 16:07:02 +0900

    Update BOJ 2981

---
+++ BOJ/2981.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int, int);
+
+int num[101];
+
+int main(){
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        cin >> num[i];
+    }
+
+    int M = 0;
+    for(int i = 1; i < N; i++){
+        M = gcd(abs(num[i] - num[i-1]), M);
+    }
+
+    set<int> result;
+    for(int i = 1; i <= sqrt(M); i++){
+        if(M % i == 0){
+            result.insert(i);
+            result.insert(M/i);
+        }
+    }
+
+    for(auto temp : result){
+        if(temp != 1){
+            cout << temp << " ";
+        }
+    }
+}
+
+int gcd(int a, int b){
+    if(b == 0){
+        return a;
+    }
+
+    return gcd(b, a % b);
+}
