f7572f5ed64cca56804d8b8e925bd9a6a5624971
yymin1022 <yymin1022@gmail.com>
2022-04-11 14:50:45 +0900

    Update BOJ 2548

---
+++ BOJ/2548.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    vector<int> nums(20000);
+    for(int i = 0; i < N; i++){
+        cin >> nums[i];
+    }
+
+    sort(nums.begin(), nums.begin() + N);
+
+    if(N % 2){
+        cout << nums[N / 2] << "\n";
+    }else{
+        cout << nums[N / 2 - 1] << "\n";
+    }
+}
