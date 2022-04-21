23b27778ae9ec5458bb368fd72b4c9996c58a297
yymin1022 <yymin1022@gmail.com>
2022-04-21 15:51:50 +0900

    Update BOJ 10986

---
+++ BOJ/10986.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long cnt[1001];
+long long sum[1000001];
+ 
+int main(void) {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, M;
+    cin >> N >> M;
+
+    long long answer = 0;
+    for(int i = 1; i <= N; i++){
+        cin >> sum[i];
+        sum[i] += sum[i - 1];
+        sum[i] %= M;
+
+        if(sum[i] == 0){
+            answer++;
+        }
+
+        cnt[sum[i]]++;
+    }
+
+    for(int i = 0; i < M; i++){
+        answer += cnt[i] * (cnt[i] - 1)/ 2;
+    }
+
+    cout << answer << "\n";
+
+    return 0;
+}
