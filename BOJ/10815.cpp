97e41aac905c0c6caedee3cd41b0a00151f927f0
yymin1022 <yymin1022@gmail.com>
2022-04-26 14:26:42 +0900

    Update BOJ 10815

---
+++ BOJ/10815.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    vector<int> cards;
+    for(int i = 0; i < N; i++){
+        int input;
+        cin >> input;
+        cards.push_back(input);
+    }
+
+    sort(cards.begin(), cards.end());
+
+    int M;
+    cin >> M;
+    for(int i = 0; i < M; i++){
+        int input;
+        cin >> input;
+
+        cout << binary_search(cards.begin(), cards.end(), input) << " ";
+    }
+
+    return 0;
+}
