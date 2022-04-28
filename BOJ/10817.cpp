1989620d40437a29b9ae2b2b50dc5a0f256adef5
yymin1022 <yymin1022@gmail.com>
2022-04-28 19:59:44 +0900

    Update BOJ 10817

--- BOJ/10817.cpp
+++ BOJ/10817.cpp
@@ -3,25 +3,14 @@
 using namespace std;
 
 int main(){
-
     vector<int> nums(3);
-
     for(int i = 0; i < 3; i++){
-
         cin >> nums[i];
-
     }
-
-    
 
     sort(nums.begin(), nums.end());
 
-    
-
     cout << nums[1] << "\n";
 
-    
-
     return 0;
-
 }
