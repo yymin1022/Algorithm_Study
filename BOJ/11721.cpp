1db71b4466afa1edb8ef6dd73b0afe2d4215fdf6
yymin1022 <yymin1022@gmail.com>
2022-05-03 20:09:06 +0900

    Update BOJ 11721

---
+++ BOJ/11721.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string word;
+    cin >> word;
+
+    for(int i = 1; i <= word.size(); i++){
+        cout << word[i - 1];
+        if(i % 10 == 0){
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
