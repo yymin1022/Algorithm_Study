0c4dc618e3efd1ce18084907c7f5499dcf896c8b
yymin1022 <yymin1022@gmail.com>
2022-05-04 10:36:55 +0900

    Update bOJ 25083

---
+++ BOJ/25083.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cout << "         ,r'\"7" << "\n";
+    cout << "r`-_   ,'  ,/" << "\n";
+    cout << " \\. \". L_r'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+
+    return 0;
+}
