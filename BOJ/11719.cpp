1b522a780b2eed2a3e647aa37ff9daee312a7c4a
yymin1022 <yymin1022@gmail.com>
2022-05-05 11:05:38 +0900

    Update BOJ 11719

---
+++ BOJ/11719.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string input;
+    while(getline(cin, input)){
+        cout << input << "\n";
+    }
+
+    return 0;
+}
