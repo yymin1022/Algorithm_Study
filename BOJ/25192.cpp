e2e2fbc259bf5a82ca1b2000694bbb0ddec0ecf4
yymin1022 <yymin1022@gmail.com>
2022-05-15 16:17:31 +0900

    Update BOJ 25192

---
+++ BOJ/25192.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    int cnt = 0;
+    set<string> chat;
+    for(int i = 0; i < N; i++){
+        string msg;
+        cin >> msg;
+
+        if(msg == "ENTER"){
+            cnt += chat.size();
+            chat.clear();
+        }else{
+            chat.insert(msg);
+        }
+    }
+
+    cnt += chat.size();
+    
+    cout << cnt << "\n";
+
+    return 0;
+}
