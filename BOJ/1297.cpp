47d83ab4148a1019ed8d5452aefe242d4aebd2f6
yymin1022 <yymin1022@gmail.com>
2022-05-17 23:16:52 +0900

    Update BOJ 1297

---
+++ BOJ/1297.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int D, H, W;
+    cin >> D >> H >> W;
+
+    double K = D / sqrt(H * H + W * W);
+    cout << (int)(H * K) << " " << (int)(W * K) << "\n";
+    
+    return 0;
+}
