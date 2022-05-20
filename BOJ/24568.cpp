278a12d8352e5f44561d20af086e89ee60da5d8d
yymin1022 <yymin1022@gmail.com>
2022-05-21 02:11:13 +0900

    Update BOJ 24568

---
+++ BOJ/24568.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int R, S;
+    cin >> R >> S;
+
+    cout << R * 8 + S * 3 - 28 << "\n";
+    
+    return 0;
+}
