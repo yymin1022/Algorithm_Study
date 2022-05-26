5a8015a8eab81d63f38562bdf0ad2b77827192e0
yymin1022 <yymin1022@gmail.com>
2022-05-26 16:43:38 +0900

    Update BOJ 2420

---
+++ BOJ/2420.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    long long N, M;
+    cin >> N >> M;
+
+    cout << abs(N - M) << "\n";
+    
+    return 0;
+}
