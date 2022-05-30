bf7b0c03694cf2c8338de610b7c31f6f2310f753
yymin1022 <yymin1022@gmail.com>
2022-05-31 04:03:45 +0900

    Update BOJ 24736

---
+++ BOJ/24736.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int T, F, S, P, C;
+    cin >> T >> F >> S >> P >> C;
+    T *= 6;
+    F *= 3;
+    S *= 2;
+    C *= 2;
+    cout << T + F + S + P + C << " ";
+
+    cin >> T >> F >> S >> P >> C;
+    T *= 6;
+    F *= 3;
+    S *= 2;
+    C *= 2;
+    cout << T + F + S + P + C << " ";
+
+    return 0;
+}
