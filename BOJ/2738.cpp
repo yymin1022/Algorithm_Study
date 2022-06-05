14cde01b2dd1e6de5bcc7f5c47135722308e6624
yymin102@ <yymin1022@gmail.com>
2022-06-06 04:45:06 +0900

    Update BOJ 2738

---
+++ BOJ/2738.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, M;
+    cin >> N >> M;
+
+    int arr[100][100];
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            int temp;
+            cin >> temp;
+            arr[i][j] += temp;
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
