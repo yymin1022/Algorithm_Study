224fb76688b51929cf7b367c8afce6b4293a2fdd
yymin102@ <yymin1022@gmail.com>
2022-06-11 04:12:30 +0900

    Update BOJ 2743

---
+++ BOJ/2743.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string word;
+    cin >> word;
+
+    cout << word.size() << "\n";
+
+    return 0;
+}
