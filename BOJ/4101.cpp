29e59450e0cf62cc9a31bf2cafa4a33f3431f57a
yymin102@ <yymin1022@gmail.com>
2022-06-24 03:56:15 +0900

    Update BOJ 4101

---
+++ BOJ/4101.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int A, B;
+    cin >> A >> B;
+
+    while(A && B){
+        if(A > B){
+            cout << "Yes" << "\n";
+        }else{
+            cout << "No" << "\n";
+        }
+
+        cin >> A >> B;
+    }
+
+    return 0;
+}
