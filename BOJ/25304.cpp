53e6dee6667db191951771533f00c4b77a44bf04
yymin102@ <yymin1022@gmail.com>
2022-06-26 02:03:43 +0900

    Update BOJ 25304

---
+++ BOJ/25304.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int X, N;
+    cin >> X >> N;
+
+    int sum = 0;
+    for(int i = 0; i < N; i++){
+        int a, b;
+        cin >> a >> b;
+
+        sum += a * b;
+    }
+
+    if(sum == X){
+        cout << "Yes" << "\n";
+    }else{
+        cout << "No" << "\n";
+    }
+
+    return 0;
+}
