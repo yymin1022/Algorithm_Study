02f14df9d6a332f1f75d79c605bb579d639540e4
yymin102@ <yymin1022@gmail.com>
2022-06-26 23:15:18 +0900

    Update BOJ 2556

---
+++ BOJ/2556.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
