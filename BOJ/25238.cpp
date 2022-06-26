72d6c28de530a348116fb04ac75cabf10031cb76
yymin102@ <yymin1022@gmail.com>
2022-06-26 22:43:28 +0900

    Update BOJ 25238

---
+++ BOJ/25238.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    double A, B;
+    cin >> A >> B;
+
+    if(A - (A * B / 100) >= 100){
+        cout << 0 << "\n";
+    }else{
+        cout << 1 << "\n";
+    }
+
+    return 0;
+}
