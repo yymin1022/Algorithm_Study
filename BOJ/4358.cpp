d10b9d42d06fcc8737c915dacb95a3afff7dd1f1
yymin1022 <yymin1022@gmail.com>
2022-07-04 21:03:26 +0900

    Update BOJ 4358

---
+++ BOJ/4358.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int cnt = 0;
+    map<string, int> tree;
+    string input;
+    while(getline(cin, input)){
+        cnt++;
+        tree[input]++;
+    }
+
+    cout << fixed;
+    cout.precision(4);
+
+    for(auto iter : tree){
+        cout << iter.first << " " << iter.second * 100 / (double)cnt << "\n";
+    }
+
+    return 0;
+}
