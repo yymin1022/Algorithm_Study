db850a1b94d17a44d16c821ea8921626499175e7
yymin102@ <yymin1022@gmail.com>
2022-07-12 03:42:33 +0900

    Update BOJ 2018

---
+++ BOJ/2018.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    int cnt = 1;
+    int left = 0;
+    int right = 1;
+    while(left < right && right < N){
+        int temp = 0;
+        for(int i = left; i <= right; i++){
+            temp += i;
+        }
+
+        if(temp <= N){
+            right++;
+        }else if(temp > N){
+            left++;
+        }
+
+        if(temp == N){
+            cnt++;
+        }
+    }
+
+    cout << cnt << "\n";
+
+    return 0;
+}
