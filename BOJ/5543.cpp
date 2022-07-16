2a2b01cd86d281aae91d676cd371036bb982bcba
yymin1022 <yymin1022@gmail.com>
2022-07-16 16:44:25 +0900

    Update BOJ 5543

---
+++ BOJ/5543.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int hamburger = 2000;
+    for(int i = 0; i < 3; i++){
+        int cost;
+        cin >> cost;
+        hamburger = min(cost, hamburger);
+    }
+
+    int drink = 2000;
+    for(int i = 0; i < 2; i++){
+        int cost;
+        cin >> cost;
+        drink = min(cost, drink);
+    }
+
+    cout << hamburger + drink - 50 << "\n";
+
+    return 0;
+}
