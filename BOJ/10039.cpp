2dd30c0cbf34a4b195ded40a4313a004800987b8
yymin1022 <yymin1022@gmail.com>
2022-07-18 02:30:15 +0900

    Update BOJ 10039

---
+++ BOJ/10039.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    int sum = 0;
+    for(int i = 0; i < 5; i++){
+        int input;
+        cin >> input;
+        
+        if(input < 40){
+            sum += 40;
+        }else{
+            sum += input;
+        }
+    }
+    
+    cout << sum / 5 << "\n";
+    
+    return 0;
+}
