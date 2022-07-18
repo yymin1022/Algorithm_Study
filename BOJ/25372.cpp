143cff28a1ac1a74a1938434ee4cf840fbf0f194
yymin1022 <yymin1022@gmail.com>
2022-07-19 04:06:40 +0900

    Update BOJ 25372

---
+++ BOJ/25372.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    int N;
+    cin >> N;
+    
+    for(int i = 0; i < N; i++){
+        string pw;
+        cin >> pw;
+        
+        if(pw.size() >= 6 && pw.size() <= 9){
+            cout << "yes" << "\n";
+        }else{
+            cout << "no" << "\n";
+        }
+    }
+    
+    return 0;
+}
