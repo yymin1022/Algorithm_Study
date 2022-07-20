f7a470603505fc1cd0bac179a0878ab262fd469e
yymin102@ <yymin1022@gmail.com>
2022-07-20 23:30:51 +0900

    Update BOJ 14465

---
+++ BOJ/14465.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N, K, B;
+    cin >> N >> K >> B;
+
+    vector<int> sum(N + 1);
+    for(int i = 0; i < B; i++){
+        int temp;
+        cin >> temp;
+        sum[temp] = 1;
+    }
+
+    for(int i = 1; i <= N; i++){
+        sum[i] += sum[i - 1];
+    }
+
+    int ans = B;
+    for(int i = 1; i <= N - K + 1; i++){
+        ans = min(sum[i + K - 1] - sum[i - 1], ans);
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
