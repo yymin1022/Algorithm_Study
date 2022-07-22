da2ea67d35325e0feae8a55bd0c50a5a996882c3
yymin102@ <yymin1022@gmail.com>
2022-07-23 01:08:22 +0900

    Update BOJ 1357

---
+++ BOJ/1357.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int rev(int);
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int X, Y;
+    cin >> X >> Y;
+
+    cout << rev(rev(X) + rev(Y)) << "\n";
+
+    return 0;
+}
+
+int rev(int num){
+    int result = 0;
+
+    while(num){
+        result *= 10;
+        result += num % 10;
+        num /= 10;
+    }
+
+    return result;
+}
