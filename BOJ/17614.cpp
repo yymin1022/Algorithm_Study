8e9c1b79cf040f41a507974d88cd9f44f60422da
yymin102@ <yymin1022@gmail.com>
2022-07-26 02:05:31 +0900

    Update BOJ 17614

---
+++ BOJ/17614.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    int cnt = 0;
+    for(int i = 1; i <= N; i++){
+        string temp = to_string(i);
+
+        for(char c : temp){
+            if(c == '3' || c == '6' || c == '9'){
+                cnt++;
+            }
+        }
+    }
+
+    cout << cnt << "\n";
+
+    return 0;
+}
