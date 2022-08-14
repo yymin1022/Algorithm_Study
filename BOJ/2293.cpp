0842b57b056af121a0496fa4199bd0958163b86b
yymin1022 <yymin1022@gmail.com>
2022-08-14 19:14:54 +0900

    Update BOJ 2293

---
+++ BOJ/2293.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[100001];
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> coin(N + 1);
+    for(int i = 1; i <= N; i++){
+        cin >> coin[i];
+    }
+
+    sort(coin.begin(), coin.end());
+
+    dp[0] = 1;
+    for(int i = 1; i <= N; i++){
+        for(int j = coin[i]; j <= K; j++){
+            dp[j] += dp[j - coin[i]];
+        }
+    }
+
+    cout << dp[K] << "\n";
+
+    return 0;
+}
