95d20753031880e290749a4bfdd4cd7fe0c7ab2e
yymin1022 <yymin1022@gmail.com>
2022-08-17 22:35:48 +0900

    Update BOj 11057

---
+++ BOJ/11057.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[1001][10];
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < 10; i++){
+        dp[1][i] = 1;
+    }
+
+    for(int i = 2; i <= N; i++){
+        for(int j = 0; j < 10; j++){
+            for(int k = 0; k <= j; k++){
+                dp[i][j] += dp[i - 1][k];
+            }
+            dp[i][j] %= 10007;
+        }
+    }
+
+    int ans = 0;
+    for(int i = 0; i < 10; i++){
+        ans += dp[N][i];
+    }
+
+    cout << ans % 10007 << "\n";
+
+    return 0;
+}
