18c7e04e08c6cf8767e388ad5437a0240f2a0cec
yymin1022 <yymin1022@gmail.com>
2022-09-19 21:27:35 +0900

    Update BOJ 25594

---
+++ BOJ/25594.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    string hg[26] = {"aespa", "baekjoon", "cau", "debug", "edge", "firefox", "golang", "haegang", "iu", "java", "kotlin", "lol", "mips", "null", "os", "python", "query", "roka", "solvedac", "tod", "unix", "virus", "whale", "xcode", "yahoo", "zebra"};
+
+    string S;
+    cin >> S;
+
+    int cur = 0;
+    string ans = "";
+    while(cur < S.size()){
+        char curC = S[cur];
+        string curHG = hg[curC - 97];
+
+        for(int i = 0; i < curHG.size(); i++){
+            if(S[cur + i] != curHG[i]){
+                cout << "ERROR!" << "\n";
+                return 0;
+            }
+        }
+
+        ans += curC;
+        cur += curHG.size();
+    }
+
+    cout << "It's HG!" << "\n";
+    cout << ans << "\n";
+
+    return 0;
+}
