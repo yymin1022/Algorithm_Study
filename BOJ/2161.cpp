a830c2260e96968390d06febc6495152bb9ae05c
yymin1022 <yymin1022@gmail.com>
2022-09-23 19:48:29 +0900

    Update BOJ 2161

---
+++ BOJ/2161.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    queue<int> Q;
+    for(int i = 1; i <= N; i++){
+        Q.push(i);
+    }
+
+    int i = 0;
+    while(!Q.empty()){
+        if(i % 2){
+            Q.push(Q.front());
+        }else{
+            cout << Q.front() << " ";
+        }
+        Q.pop();
+        i++;
+    }
+
+    return 0;
+}
