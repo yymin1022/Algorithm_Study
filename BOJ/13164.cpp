62f2ea9a3f5642ab9fc2c186fe939b99f67cddbf
yymin1022 <yymin1022@gmail.com>
2022-09-29 15:29:16 +0900

    Update BOJ 13164

---
+++ BOJ/13164.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    int N, K;
+    cin >> N >> K;
+
+    int ans = 0;
+    vector<int> diff;
+    vector<int> student;
+    for(int i = 0; i < N; i++){
+        int input;
+        cin >> input;
+        student.push_back(input);
+
+        if(i > 0){
+            ans += student[i] - student[i - 1];
+            diff.push_back(student[i] - student[i - 1]);
+        }
+    }
+
+    sort(diff.begin(), diff.end(), greater<int>());
+
+    for(int i = 0; i < K - 1; i++){
+        ans -= diff[i];
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
