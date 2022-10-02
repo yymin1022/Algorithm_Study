1d6f85fef50536817759f4c6752cffaca50f2282
yymin1022 <yymin1022@gmail.com>
2022-10-02 16:35:41 +0900

    Update BOJ 14581

---
+++ BOJ/14581.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string id;
+    cin >> id;
+
+    cout << ":fan::fan::fan:" << "\n";
+    cout << ":fan::" << id << "::fan:" << "\n";
+    cout << ":fan::fan::fan:" << "\n";
+
+    return 0;
+}
