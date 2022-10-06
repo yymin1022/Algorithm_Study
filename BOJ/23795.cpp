4ddd4eab4f4baee051be0e3594d87938748f8d6f
yymin1022 <yymin1022@gmail.com>
2022-10-07 00:41:38 +0900

    Update BOJ 23795

---
+++ BOJ/23795.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int sum = 0;
+    while(true){
+        int input;
+        cin >> input;
+
+        if(input < 0){
+            break;
+        }
+
+        sum += input;
+    }
+
+    cout << sum << "\n";
+
+    return 0;
+}
