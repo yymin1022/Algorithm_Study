4ae68b6878d5f6c7f9485fbed9bf983d461ab886
yymin1022 <yymin1022@gmail.com>
2022-10-10 02:26:20 +0900

    Update BOJ 13866

---
+++ BOJ/13866.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int A, B, C, D;
+    cin >> A >> B >> C >> D;
+
+    cout << abs(A + D - B - C) << "\n";
+
+    return 0;
+}
