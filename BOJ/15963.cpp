938a87e78b715963cb9907d00c08560fa9313d32
yymin1022 <yymin1022@gmail.com>
2022-10-10 23:35:11 +0900

    Update BOJ 15963

---
+++ BOJ/15963.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    long long N, M;
+    cin >> N >> M;
+
+    if(N == M){
+        cout << 1 << "\n";
+    }else{
+        cout << 0 << "\n";
+    }
+
+    return 0;
+}
