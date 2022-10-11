91b13c7aa425564cdcf9e003b7665f5d5ea300d6
yymin1022 <yymin1022@gmail.com>
2022-10-12 00:50:22 +0900

    Update BOJ 11948

---
+++ BOJ/11948.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int minS = 100;
+    int sum = 0;
+    for(int i = 0; i < 4; i++){
+        int input;
+        cin >> input;
+        minS = min(input, minS);
+        sum += input;
+    }
+
+    sum -= minS;
+    minS = 100;
+
+    for(int i = 0; i < 2; i++){
+        int input;
+        cin >> input;
+
+        minS = min(input, minS);
+        sum += input;
+    }
+
+    sum -= minS;
+
+    cout << sum << "\n";
+
+    return 0;
+}
