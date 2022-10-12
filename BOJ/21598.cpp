6aa279c00826e47725c77faf800a8c31eca8af4d
yymin1022 <yymin1022@gmail.com>
2022-10-13 00:40:28 +0900

    Update BOJ 21598

---
+++ BOJ/21598.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    long long N;
+    cin >> N;
+
+    for(long long i = 0; i < N; i++){
+        cout << "SciComLove" << "\n";
+    }
+
+    return 0;
+}
