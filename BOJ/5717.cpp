d7087066130977f761ba013f938648597c4dc8d4
yymin1022 <yymin1022@gmail.com>
2022-10-15 03:19:31 +0900

    Update BOJ 5717

---
+++ BOJ/5717.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int M, F;
+    cin >> M >> F;
+
+    while(M && F){
+        cout << M + F << "\n";
+
+        cin >> M >> F;
+    }
+
+    return 0;
+}
