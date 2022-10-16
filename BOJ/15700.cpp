a99862ac51b3b72381e360a94663869ffaa39feb
yymin1022 <yymin1022@gmail.com>
2022-10-16 22:29:58 +0900

    Update BOJ 15700

---
+++ BOJ/15700.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    long long N, M;
+    cin >> N >> M;
+
+    cout << (N * M) / 2 << "\n";
+
+    return 0;
+}
