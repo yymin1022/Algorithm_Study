502497f24359aaa1420d98818b231ff65f9ecc9f
yymin1022 <yymin1022@gmail.com>
2022-10-18 00:20:14 +0900

    Update BOJ 9316

---
+++ BOJ/9316.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    for(int i = 1; i <= N; i++){
+        cout << "Hello World, Judge " << i << "!" << "\n";
+    }
+
+    return 0;
+}
