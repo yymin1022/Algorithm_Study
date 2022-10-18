945bce6a4532df8bd4a7e8496f6c1edaec053676
yymin1022 <yymin1022@gmail.com>
2022-10-19 01:23:58 +0900

    Update BOJ 19698

---
+++ BOJ/19698.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N, W, H, L;
+    cin >> N >> W >> H >> L;
+
+    int ans = min((W / L) * (H / L), N);
+    cout << ans << "\n";
+
+    return 0;
+}
