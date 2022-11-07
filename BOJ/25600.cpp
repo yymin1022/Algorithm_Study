ed90b38e0559e9638e3e164fea89d23b71302514
yymin1022 <yymin1022@gmail.com>
2022-11-08 02:48:47 +0900

    Update BOJ 25600

---
+++ BOJ/25600.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    int ans = 0;
+    for(int i = 0; i < N; i++){
+        int a, d, g;
+        cin >> a >> d >> g;
+
+        int temp = a * (d + g);
+
+        if(a == d + g){
+            temp *= 2;
+        }
+
+        ans = max(temp, ans);
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
