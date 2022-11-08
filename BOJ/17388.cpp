fda5e38ac1900f244b42d73bcd18babbe0730700
yymin1022 <yymin1022@gmail.com>
2022-11-08 17:44:01 +0900

    Update BOJ 17388

---
+++ BOJ/17388.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    int S, K, H;
+    cin >> S >> K >> H;
+
+    if(S + K + H >= 100){
+        cout << "OK" << "\n";
+    }else{
+        if(S < K){
+            if(S < H){
+                cout << "Soongsil" << "\n";
+            }else{
+                cout << "Hanyang" << "\n";
+            }
+        }else{
+            if(K < H){
+                cout << "Korea" << "\n";
+            }else{
+                cout << "Hanyang" << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
