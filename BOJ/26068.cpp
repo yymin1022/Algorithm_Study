0ad2f5fb0562e29d99d3fa6270f866478a2ef07d
yymin1022 <yymin1022@gmail.com>
2022-11-28 00:46:48 +0900

    Update BOJ 26068

---
+++ BOJ/26068.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    int ans = 0;
+    for(int i = 0; i < N; i++){
+        string input;
+        cin >> input;
+
+        string temp = input.substr(2, input.size() - 2);
+        if(stoi(temp) <= 90){
+            ans++;
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
