29d4374493373aa44876ec0fe5c3cd66932d3702
yymin1022 <yymin1022@gmail.com>
2022-12-03 17:50:02 +0900

    Update BOJ 10995

---
+++ BOJ/10995.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        if(i % 2){
+            cout << " ";
+        }
+
+        for(int j = 0; j < N; j++){
+            cout << "* ";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
