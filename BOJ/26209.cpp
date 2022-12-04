ccaee02a1133d143b1672eb3100379b1c20a40c0
yymin1022 <yymin1022@gmail.com>
2022-12-04 14:32:31 +0900

    Update BOJ 26209

---
+++ BOJ/26209.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    for(int i = 0; i < 8; i++){
+        int input;
+        cin >> input;
+
+        if(input == 9){
+            cout << "F" << "\n";
+            return 0;
+        }
+    }
+
+    cout << "S" << "\n";
+
+    return 0;
+}
