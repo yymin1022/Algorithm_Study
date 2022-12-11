d477a89d4127a4a3414601224531656168bb11ae
yymin1022 <yymin1022@gmail.com>
2022-12-11 14:31:06 +0900

    Update BOJ 5565

---
+++ BOJ/5565.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < 9; i++){
+        int input;
+        cin >> input;
+        N -= input;
+    }
+
+    cout << N << "\n";
+
+    return 0;
+}
