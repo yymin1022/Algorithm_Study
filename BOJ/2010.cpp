8f2a02f8dc43b339dd6591cf6caa1ef7200e594d
yymin1022 <yymin1022@gmail.com>
2022-12-12 13:13:12 +0900

    Update BOJ 2010

---
+++ BOJ/2010.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    int N;
+    cin >> N;
+
+    int ans;
+    cin >> ans;
+
+    for(int i = 1; i < N; i++){
+        int input;
+        cin >> input;
+
+        ans += input - 1;
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
