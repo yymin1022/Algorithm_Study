feb221246ea86b0709ae67ab8fe3c8400a917451
yymin1022 <yymin1022@gmail.com>
2022-12-23 02:16:51 +0900

    Update BOJ 10833

---
+++ BOJ/10833.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    int ans = 0;
+    for(int i = 0; i < N; i++){
+        int stu, app;
+        cin >> stu >> app;
+
+        ans += app % stu;
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
