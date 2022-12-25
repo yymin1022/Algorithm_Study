00d8fc0f887037f15ac37f19125447069148f1ba
yymin1022 <yymin1022@gmail.com>
2022-12-25 20:28:14 +0900

    Update BOJ 15904

---
+++ BOJ/15904.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    string S;
+    getline(cin, S);
+
+    int idx = 0;
+    string tmp = "";
+    string UCPC = "UCPC";
+    for(int i = 0; i < S.size(); i++){
+        if(S[i] == UCPC[idx]){
+            idx++;
+            tmp += S[i];
+
+            idx %= 4;
+        }
+
+        if(tmp == UCPC){
+            cout << "I love UCPC" << "\n";
+            return 0;
+        }
+    }
+
+    cout << "I hate UCPC" << "\n";
+    
+    return 0;
+}
