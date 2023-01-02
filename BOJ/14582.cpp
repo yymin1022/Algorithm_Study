46ee59176735ea3dcb134a775d4656f680e7dd55
yymin1022 <yymin1022@gmail.com>
2023-01-02 16:53:13 +0900

    Update BOJ 14582

---
+++ BOJ/14582.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    vector<int> wool(10, 0);
+    for(int i = 1; i <= 9; i++){
+        int input;
+        cin >> input;
+        wool[i] = wool[i - 1] + input;
+    }
+
+    vector<int> star(10, 0);
+    for(int i = 1; i <= 9; i++){
+        int input;
+        cin >> input;
+        star[i] = star[i - 1] + input;
+    }
+
+    bool isWool = false;
+    for(int i = 1; i <= 9; i++){
+        if(wool[i] > star[i - 1]){
+            isWool = true;
+            break;
+        }
+    }
+
+    cout << (isWool ? "Yes" : "No") << "\n";
+
+    return 0;
+}
