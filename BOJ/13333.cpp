d2a673a21aa98e5523cd90ab2f19f3d18f458809
yymin1022 <yymin1022@gmail.com>
2023-01-27 00:51:21 +0900

    Update BOJ 13333

---
+++ BOJ/13333.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    vector<int> docs;
+    for(int i = 0; i < N; i++){
+        int input;
+        cin >> input;
+        docs.push_back(input);
+    }
+
+    sort(docs.begin(), docs.end());
+
+    for(int i = 0; i <= N; i++){
+        int tmp = docs.end() - lower_bound(docs.begin(), docs.end(), i);
+        if(tmp >= i && docs[N - i - 1] <= i){
+            cout << i << "\n";
+        }
+    }
+
+    return 0;
+}
