d90703663c0a5b3675405bf4af01708f6d4eb3dc
yymin1022 <yymin1022@gmail.com>
2023-02-03 23:10:16 +0900

    Update BOJ 26574

---
+++ BOJ/26574.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        int input;
+        cin >> input;
+        cout << input << " " << input << "\n";
+    }
+
+    return 0;
+}
