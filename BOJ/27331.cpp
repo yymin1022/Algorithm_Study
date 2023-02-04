7b5eeecb85e7c5f9b6b0e87b26547b48d594fa0a
yymin1022 <yymin1022@gmail.com>
2023-02-05 01:13:48 +0900

    Update BOJ 27331

---
+++ BOJ/27331.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    char a, b;
+    cin >> a >> b;
+
+    cout << a << b << "\n";
+
+    return 0;
+}
