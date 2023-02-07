1564c2a283cd5765ad48a7ec484689b1ecf11051
yymin1022 <yymin1022@gmail.com>
2023-02-08 02:18:51 +0900

    Update BOJ 27294

---
+++ BOJ/27294.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int T, S;
+    cin >> T >> S;
+
+    if(T >= 12 && T <= 16 && S == 0){
+        cout << 320 << "\n";
+    }else{
+        cout << 280 << "\n";
+    }
+
+    return 0;
+}
