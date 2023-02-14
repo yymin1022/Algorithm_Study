9a93b04ef0570a427bc84171149e4d1222fedea5
yymin1022 <yymin1022@gmail.com>
2023-02-14 14:04:11 +0900

    Update BOJ 26575

---
+++ BOJ/26575.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    double N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        double D, F, P;
+        cin >> D >> F >> P;
+        cout.precision(2);
+        cout << fixed;
+        cout << "$" << D * F * P << "\n";
+    }
+
+    return 0;
+}
