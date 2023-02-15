74bc52e222a7be9e0c1adac5fc17e40c49c96c0c
yymin1022 <yymin1022@gmail.com>
2023-02-16 00:58:06 +0900

    Update BOJ 18698

---
+++ BOJ/18698.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+
+    while(T--){
+        string input;
+        cin >> input;
+
+        int cnt = 0;
+        for(int i = 0; i < input.size(); i++){
+            if(input[i] == 'U'){
+                cnt++;
+            }else{
+                break;
+            }
+        }
+
+        cout << cnt << "\n";
+    }
+
+    return 0;
+}
