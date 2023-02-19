81c1ae348074044484958f3f2552fa2d9a8f6374
yymin1022 <yymin1022@gmail.com>
2023-02-19 16:50:40 +0900

    Update BOJ 9625

---
+++ BOJ/9625.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int K;
+    cin >> K;
+
+    vector<pair<int, int>> dp;
+    dp.push_back(make_pair(1, 0));
+    for(int i = 1; i <= K; i++) {
+        int cntA = dp[i - 1].first;
+        int cntB = dp[i - 1].second;
+
+        int newA = cntB;
+        int newB = cntA + cntB;
+
+        dp.push_back(make_pair(newA, newB));
+    }
+
+    cout << dp[K].first << " " << dp[K].second << "\n";
+
+    return 0;
+}
