4e10c3fbece8f1db1046b8aff2a7f88eeb27c5f1
yymin1022 <yymin1022@gmail.com>
2023-02-28 03:47:11 +0900

    Update BOJ 11549

---
+++ BOJ/11549.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+
+    int ans = 0;
+    for(int i = 0; i < 5; i++){
+        int input;
+        cin >> input;
+        if(T == input){
+            ans++;
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
