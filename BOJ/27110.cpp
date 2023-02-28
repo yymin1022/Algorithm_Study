c2e8a97cc22ee454dfbe1a8698fe008e4ec9067a
yymin1022 <yymin1022@gmail.com>
2023-03-01 03:26:38 +0900

    Update BOJ 27110

---
+++ BOJ/27110.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    int ans = 0;
+    for(int i = 0; i < 3; i++){
+        int input;
+        cin >> input;
+
+        if(input <= N){
+            ans += input;
+        }else{
+            ans += N;
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
