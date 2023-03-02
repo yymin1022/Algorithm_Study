900d11857b81e328c396a149090a1dcf51e5934a
yymin1022 <yymin1022@gmail.com>
2023-03-03 03:56:43 +0900

    Update BOJ 21633

---
+++ BOJ/21633.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    float k;
+    cin >> k;
+
+    k /= 100;
+    k += 25;
+
+    cout.precision(2);
+    cout << fixed;
+
+    if(k < 100){
+        cout << 100.0f << "\n";
+    }else if(k > 2000){
+        cout << 2000.0f << "\n";
+    }else{
+        cout << k << "\n";
+    }
+
+    return 0;
+}
