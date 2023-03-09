c983fd2339068665862966f4e2e3548ff118236d
yymin1022 <yymin1022@gmail.com>
2023-03-10 02:11:39 +0900

    Update BOJ 27866

---
+++ BOJ/27866.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    string S;
+    int i;
+    cin >> S >> i;
+    cout << S[i - 1] << "\n";
+
+    return 0;
+}
