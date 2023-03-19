18b7dcf6188dbbe34eaa9c488c46c4ecf7c6deef
yymin1022 <yymin1022@gmail.com>
2023-03-20 02:44:53 +0900

    Update BOJ 2875

---
+++ BOJ/2875.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    while(K--){
+        if(N > M * 2){
+            N--;
+        }else{
+            M--;
+        }
+    }
+
+    int ans = M;
+    if(N < M * 2){
+        ans = N / 2;
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
