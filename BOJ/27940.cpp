7727b1d29820d002addad60d30e5f116f67074c9
yymin1022 <yymin1022@gmail.com>
2023-04-17 00:23:18 +0900

    Update BOJ 27940

---
+++ BOJ/27940.cpp
@@ -0,0 +1,30 @@
+#include  <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    int sum = 0;
+    for(int i = 1; i <= M; i++){
+        int t, r;
+        cin >> t >> r;
+
+        sum += r;
+
+        if(sum > K){
+            cout << i << " ";
+            cout << 1 << "\n";
+            return 0;
+        }
+    }
+
+    cout << -1 << "\n";
+
+    return 0;
+}
