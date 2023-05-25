d3155ba914aaea1ab39d613405cfaa9af658e970
yymin1022 <yymin1022@gmail.com>
2023-05-25 21:47:39 +0900

    Update BOJ 68\40

---
+++ BOJ/6840.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int arr[3];
+    cin >> arr[0] >> arr[1] >> arr[2];
+
+    sort(arr, arr + 3);
+
+    cout << arr[1] << "\n";
+
+    return 0;
+}
