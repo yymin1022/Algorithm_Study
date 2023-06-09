71ff3bf0995e315e6adb5cb66c33ead725e28bec
Yymin1022 <yymin1022@gmail.com>
2023-06-09 17:39:13 +0900

    Update BOJ 15688

---
+++ BOJ/15688.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    vector<int> arr;
+    for(int i = 0; i < N; i++){
+        int input;
+        cin >> input;
+        arr.push_back(input);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for(int i = 0; i < N; i++){
+        cout << arr[i] << "\n";
+    }
+
+    return 0;
+}
