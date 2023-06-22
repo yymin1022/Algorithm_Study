af6ffc1a916a2362cf3c24c53bfbe6d64df5d33b
yymin1022 <yymin1022@gmail.com>
2023-06-23 00:32:22 +0900

    Update BOJ 20529

---
+++ BOJ/20529.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+
+    while(T--){
+        int N;
+        cin >> N;
+        
+        vector<string> mbti;
+        for(int i = 0; i < N; i++){
+            string input;
+            cin >> input;
+            mbti.push_back(input);
+        }
+
+        if(N > 32){
+            cout << 0 << "\n";
+            continue;
+        }
+
+        int ans = 2147483647;
+        for(int i = 0; i < N; i++){
+            for(int j = i + 1; j < N; j++){
+                for(int k = j + 1; k < N; k++){
+                    int tmp = 0;
+                    for(int m = 0; m < 4; m++){
+                        tmp += mbti[i][m] != mbti[j][m];
+                        tmp += mbti[j][m] != mbti[k][m];
+                        tmp += mbti[k][m] != mbti[i][m];
+                    }
+                    ans = min(tmp, ans);
+                }
+            }
+        }
+
+        cout << ans << "\n";
+    }
+}
