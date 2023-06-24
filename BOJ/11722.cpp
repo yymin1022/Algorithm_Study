d3b95787815cfddc1c44023acd82e74bfcb2f82c
yymin1022 <yymin1022@gmail.com>
2023-06-24 15:27:52 +0900

    Update BOJ 11722

---
+++ BOJ/11722.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int A[1001];
+int dp[1001];
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        cin >> A[i];
+    }
+
+    int ans = 0;
+    for(int i = 0; i < N; i++){
+        dp[i] = 1;
+
+        for(int j = 0; j < i; j++){
+            if(A[i] < A[j]){
+                dp[i] = max(dp[j] + 1, dp[i]);
+            }
+        }
+        ans = max(dp[i], ans);
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
