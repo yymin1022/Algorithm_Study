87c095ce64a23035534e87b0c3e082c5c7470ba3
yymin1022 <yymin1022@gmail.com>
2023-07-27 04:16:48 +0900

    Update BOJ 9372

---
+++ BOJ/9372.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+
+    while(T--){
+        int N, M;
+        cin >> N >> M;
+
+        for(int i = 0; i < M; i++){
+            int A, B;
+            cin >> A >> B;
+        }
+
+        cout << N - 1 << "\n";
+    }
+
+    return 0;
+}
