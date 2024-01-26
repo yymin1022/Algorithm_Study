e62aab1c182f6c0c61cfd545fbd7e7e4c2811a79
yymin1022 <yymin1022@gmail.com>
2024-01-27 05:48:47 +0900

    Update BOJ 20004

---
+++ BOJ/20004.cpp
@@ -0,0 +1,20 @@
+#include  <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int A;
+    cin >> A;
+
+    for(int i = 1; i <= A; i++){
+        if(31 % (i + 1) == 1){
+            cout << i << "\n";
+        }
+    }
+
+    return 0;
+}
