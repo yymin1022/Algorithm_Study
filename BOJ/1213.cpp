6cc6df1207f9e9470e5f907fac5e530d79d5c104
yymin1022 <yymin1022@gmail.com>
2024-03-03 21:54:24 +0900

    Update BOJ 1213

---
+++ BOJ/1213.cpp
@@ -0,0 +1,50 @@
+#include  <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string S;
+    cin >> S;
+
+    vector<int> cnt(26, 0);
+    for(int i = 0; i < S.size(); i++){
+        cnt[S[i] - 'A']++;
+    }
+
+    int oddCnt = 0;
+    for(int i = 0; i < 26; i++){
+        oddCnt += cnt[i] % 2;
+    }
+
+    if(oddCnt > 1){
+        cout << "I'm Sorry Hansoo" << "\n";
+        return 0;
+    }
+
+    string ans;
+    for(int i = 0; i < 26; i++){
+        for(int j = 0; j < cnt[i] / 2; j++){
+            ans.push_back(i + 'A');
+        }
+    }
+
+    for(int i = 0; i < 26; i++){
+        if(cnt[i] % 2){
+            ans.push_back(i + 'A');
+        }
+    }
+
+    for(int i = 25; i >= 0; i--){
+        for(int j = 0; j < cnt[i] / 2; j++){
+            ans.push_back(i + 'A');
+        }
+    }
+
+    cout << ans <<'\n';
+
+    return 0;
+}
