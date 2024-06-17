5a1d5213040c5d5eda9b9aaba5dd948fbdc98364
yymin1022 <yymin1022@gmail.com>
2024-06-18 01:31:37 +0900

    Update BOJ 31403

---
+++ BOJ/31403.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    string A, B;
+    int C;
+    cin >> A >> B >> C;
+
+    cout << stoi(A) + stoi(B) - C << "\n";
+    cout << stoi(A + B) - C << "\n";
+
+    return 0;
+}
