deefb22092d7f90ef69e2d314e0f24c0d78b3160
yymin1022 <yymin1022@gmail.com>
2024-07-23 15:02:07 +0900

    Update BOJ 3211

---
+++ BOJ/3211.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    vector<int> friends;
+    for(int i = 0; i < N; i++){
+        int input;
+        cin >> input;
+        friends.push_back(input);
+    }
+
+    sort(friends.begin(), friends.end());
+
+    int ans = 1;
+    while(ans <= N && ans < friends[ans - 1] + 1){
+        ans = friends[ans - 1] + 1;
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
