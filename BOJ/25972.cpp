9d44683bc5af6c68b031a7472f07cdfbe43634f4
yymin1022 <yymin1022@gmail.com>
2024-07-30 22:55:59 +0900

    Update BOJ 25972

---
+++ BOJ/25972.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> arr;
+    for(int i = 0; i < N; i++){
+        int A, B;
+        cin >> A >> B;
+        arr.push_back(make_pair(A, B));
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int ans = 0;
+    int tmp = -1;
+    for(int i = 0; i < N; i++){
+        if(tmp < arr[i].first){
+            ans++;
+        }
+
+        tmp = arr[i].first + arr[i].second;
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
