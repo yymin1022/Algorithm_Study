a294337156b8998278325b6585e1b423b3910f49
yymin1022 <yymin1022@gmail.com>
2024-08-10 23:39:07 +0900

    Update BOJ 7515

---
+++ BOJ/7515.py
@@ -0,0 +1,10 @@
+dp = [1, 1]
+for i in range(2, 50):
+    dp.append(dp[i - 1] + dp[i - 2])
+
+N = int(input())
+
+for i in range(0, N):
+    S = int(input())
+
+    print(f"Scenario {i + 1}:\n{dp[S + 1]}\n")
