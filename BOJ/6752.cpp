000dff91db6d5df6640a1c917400c4b5dc725ab7
yymin1022 <yymin1022@gmail.com>
2024-08-12 14:45:42 +0900

    Update BOJ 6752

---
+++ BOJ/6752.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int T, C;
+    cin >> T >> C;
+
+    vector<int> task;
+    for(int i = 0; i < C; i++){
+        int input;
+        cin >> input;
+        task.push_back(input);
+    }
+
+    sort(task.begin(), task.end());
+
+    int cnt = 0;
+    int sum = 0;
+    for(int i = 0; i < C; i++){
+        if(task[i] <= T - sum){
+            cnt++;
+            sum += task[i];
+        }else{
+            break;
+        }
+    }
+
+    cout << cnt << "\n";
+
+    return 0;
+}
