ae39e2ba99a1b8a86eb2c666df2bacfff277329f
yymin1022 <yymin1022@gmail.com>
2024-08-17 10:56:33 +0900

    Update BOJ 23278

---
+++ BOJ/23278.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, L, H;
+    cin >> N >> L >> H;
+
+    vector<int> arr;
+    for(int i = 0; i < N; i++){
+        int input;
+        cin >> input;
+        arr.push_back(input);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    double sum = 0;
+    for(int i = L; i < N - H; i++){
+        sum += arr[i];
+    }
+
+    cout.precision(10);
+    cout << fixed << sum / (N - L - H) << "\n";
+
+    return 0;
+}
