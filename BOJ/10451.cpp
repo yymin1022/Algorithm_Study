e9673ee909eac1f51b1c71e1c23ada529eadfd44
yymin1022 <yymin1022@gmail.com>
2024-08-31 02:20:53 +0900

    Update BOJ 10451

---
+++ BOJ/10451.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isVisit[1001];
+int graph[1001];
+int N;
+
+void dfs(int cur){
+    isVisit[cur] = true;
+
+    if(!isVisit[graph[cur]]){
+        dfs(graph[cur]);
+    }
+}
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+
+    while(T--){
+        cin >> N;
+
+        for(int i = 1; i <= N; i++){
+            cin >> graph[i];
+        }
+
+        int ans = 0;
+        memset(isVisit, false, sizeof(isVisit));
+        for(int i = 1; i <= N; i++){
+            if(!isVisit[i]){
+                dfs(i);
+                ans++;
+            }
+        }
+
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
