381831b06d20db938ec1afae8ef9e7a951322147
yymin1022 <yymin1022@gmail.com>
2024-12-01 17:14:06 +0900

    Update BOJ 27961

---
+++ BOJ/27961.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    long long N;
+    cin >> N;
+
+    if(N == 0){
+        cout << 0 << "\n";
+        return 0;
+    }
+
+    long long ans = 1;
+    long long cnt = 1;
+    while(cnt < N){
+        cnt *= 2;
+        ans++;
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
