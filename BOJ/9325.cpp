4965f7acc30170a17409d795afa0a66f77ec0317
yymin1022 <yymin1022@gmail.com>
2024-12-02 23:06:30 +0900

    Update BOJ 9325

---
+++ BOJ/9325.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+
+    while(T--){
+        int S, N;
+        cin >> S >> N;
+
+        while(N--){
+            int P, Q;
+            cin >> P >> Q;
+            S += P * Q;
+        }
+
+        cout << S << "\n";
+    }
+
+    return 0;
+}
