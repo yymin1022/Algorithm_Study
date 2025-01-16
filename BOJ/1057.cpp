58fa9b1ba3dc29b8fcc7e5f9a306478bb9dbda2f
yymin1022 <yymin1022@gmail.com>
2025-01-16 23:45:33 +0900

    Update BOj 1057

---
+++ BOJ/1057.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, A, B;
+    cin >> N >> A >> B;
+
+    int ans = 1;
+    while(true){
+        if(A % 2 == 1){
+            A++;
+        }
+
+        if(B % 2 == 1){
+            B++;
+        }
+
+        if(A == B){
+            break;
+        }
+
+        ans++;
+        A /= 2;
+        B /= 2;
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
