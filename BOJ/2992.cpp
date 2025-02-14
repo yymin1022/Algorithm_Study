50650facd92a3332e0980ba33d5860a4006b6eb5
yymin1022 <yymin1022@gmail.com>
2025-02-15 01:36:41 +0900

    Update BOJ 2992

---
+++ BOJ/2992.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string S;
+    cin >> S;
+
+    if(next_permutation(S.begin(), S.end())){
+        cout << S << "\n";
+    }else{
+        cout << 0 << "\n";
+    }
+
+    return 0;
+}
