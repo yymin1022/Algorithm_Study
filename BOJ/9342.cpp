ec3ecdc0a4205d33c68b02481744f9cb24d509a1
yymin1022 <yymin1022@gmail.com>
2025-02-20 23:04:49 +0900

    Update BOJ 9342

---
+++ BOJ/9342.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    regex reg(R"(^[A-F]?A+F+C+[A-F]?$)");
+
+    int T;
+    cin >> T;
+
+    while(T--){
+        string S;
+        cin >> S;
+
+        cout << (regex_match(S, reg) ? "Infected!" : "Good") << "\n";
+    }
+
+    return 0;
+}
