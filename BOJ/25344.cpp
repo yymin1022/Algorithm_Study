f609e306a027b4e7f44fc5a85a4187e3b3b85d14
yymin1022 <yymin1022@gmail.com>
2025-03-08 22:50:45 +0900

    Update BOJ 25344

---
+++ BOJ/25344.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long gcd(long long A, long long B){
+    if(B == 0){
+        return A;
+    }
+
+    return gcd(B, A % B);
+}
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    long long ans = 1;
+    for(int i = 0; i < N - 2; i++){
+        long long input;
+        cin >> input;
+
+        ans *= input / gcd(ans, input);
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
